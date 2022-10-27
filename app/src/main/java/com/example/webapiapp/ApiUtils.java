318645d75937e2e699c567598c5e869b5d86941a
imtgunes <imtgunes@hotmail.com>
2022-10-27 14:30:25 +0300

    Android app

--- app/src/main/java/com/example/webapiapp/ApiUtils.java
+++ app/src/main/java/com/example/webapiapp/ApiUtils.java
@@ -1,7 +1,7 @@
 package com.example.webapiapp;
 
 public class ApiUtils {
-    public static final String BASE_URL = "https://448a-195-177-206-66.eu.ngrok.io/";
+    public static final String BASE_URL = "https://b6af-195-177-206-66.eu.ngrok.io/";
 
     public static ApiInterface getApi(){
         return  ApiClient.getRetrofit(BASE_URL).create(ApiInterface.class);
